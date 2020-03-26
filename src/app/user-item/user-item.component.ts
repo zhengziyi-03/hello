3f85a6a69b8957f93dab4b93d0d61707817f5027
zhengziyi <1473498140@qq.com>
2020-03-26 22:02:57 +0800

    hello

---
+++ src/app/user-item/user-item.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-user-item',
+  templateUrl: './user-item.component.html',
+  styleUrls: ['./user-item.component.css']
+})
+export class UserItemComponent implements OnInit {
+  @Input() name2: string;
+
+  constructor() {
+    
+   }
+
+  ngOnInit(): void {
+  }
+
+}
